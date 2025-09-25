04af303714e2adedfc151ff9475c0499a856dc77
Tim Galvin <gal16b@petrichor-i3.cm.cluster>
2025-09-25 18:24:56 +1000

    updated the convert script to be inplace

--- convert.py
+++ convert.py
@@ -1,10 +1,12 @@
-# Very rough script to convert a time step fits cube from the 
-# subtract cube pipeline workflow so that is can be opened in 
-# carta and the like. The smarter thing to do would be to update
-# the header as below, but write out the header with the 'as_string()'
-# method directly to the start of the fits file in place. 
-# This script will be added to the auto_vast repo for history and a 
-# improved version will be added
+"""
+Update the FITS header to remove unnecessary fits axis information. Importantly
+this script makes the modification in place, and allows the time axis to be
+treated the same as the frequency axis would in a spectral cube.
+
+The modificaiton to the FITS header is down by writing to the file 
+directly. This is of huge benefit for large cubes.
+""" 
+
 from pathlib import Path 
 import sys
 
@@ -12,11 +14,13 @@ from astropy.io import fits
 import numpy as np
 
 fits_in = Path(sys.argv[1])
-fits_out = fits_in.with_suffix(".clean.fits")
 
 print("Getting Header")
 
 hdr = fits.getheader(fits_in)
+original_string = hdr.tostring()
+
+print(f"Length of header keys: {len(hdr)}")
 
 print("Popping keys")
 to_pop = []
@@ -41,8 +45,38 @@ hdr["NAXIS3"] = hdr["NAXIS5"]
 hdr.pop("NAXIS4")
 hdr.pop("NAXIS5")
 
-new_data = np.squeeze(fits.getdata(fits_in))
+print(f"Length of header keys: {len(hdr)}")
 
-print(f"Writing {fits_out=}")
-fits.writeto(fits_out, header=hdr, data=new_data, overwrite=True)
+new_hdr_string = hdr.tostring()
 
+difference = len(original_string) - len(new_hdr_string)
+print(f"Original header length: {len(original_string)}")
+print(f"Updated header length: {len(new_hdr_string)}")
+print(f"Diff. to pad: {difference}")
+
+new_hdr_string += " "*difference
+
+if len(new_hdr_string) != len(original_string):
+    msg = "Header string mis-match"
+    raise ValueError(msg)
+
+if new_hdr_string == original_string:
+    msg = "There should be some difference"
+    raise ValueError(msg)
+
+print("\bModifying file in place")
+
+assert "CDELT4" not in new_hdr_string, "WHAT"
+
+print(f"Modying {fits_in}")
+with open(fits_in, 'rb+') as out_file:
+    # First we blank out the existing header
+    print("... Blanking out existing header")
+    out_file.seek(0)
+    out_file.write(b" "*len(original_string))
+    
+    # Now we write out the new header
+    print("... Writing out new header")
+    out_file.seek(0)
+    out_file.write(new_hdr_string.encode('ascii'))
+    out_file.flush()
